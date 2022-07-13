87d434ceb97b8a9bb15a2373dd4e6476454dc959
Abhishek Mandilkar <a.a.mandilkar@gmail.com>
2022-07-13 17:19:07 +0530

    fix deployement issue

--- src/components/NotesContainer/index.js
+++ src/components/NotesContainer/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { getAllNotes } from "../../features/notes/noteSlice";
 import NoNotes from "../NoNotes";
